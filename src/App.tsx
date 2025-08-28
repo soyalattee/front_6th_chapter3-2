f8cc33420cef47132b9abf2dd6c9547201fd2f69
soyalattee <uyyu1734@gmail.com>
2025-08-29 01:20:32 +0900

    test: 알림설정 통합테스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -472,7 +472,6 @@ function App() {
               label="반복 일정"
             />
           </FormControl>
-
           <FormControl fullWidth>
             <FormLabel htmlFor="notification">알림 설정</FormLabel>
             <Select
@@ -480,9 +479,10 @@ function App() {
               size="small"
               value={notificationTime}
               onChange={(e) => setNotificationTime(Number(e.target.value))}
+              aria-label="알림 설정"
             >
               {notificationOptions.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
+                <MenuItem aria-label={option.label} key={option.value} value={option.value}>
                   {option.label}
                 </MenuItem>
               ))}
